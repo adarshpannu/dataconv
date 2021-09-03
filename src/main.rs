0045b69b079928a6c2bc58938f678cf2ab8bd7a4
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2021-09-02 23:14:35 -0700

    Quick checkpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("datautils")
+
+use arrow2::error::Result;
+use arrow2::io::csv::read;
+use arrow2::record_batch::RecordBatch;
+
+fn read_path(path: &str, projection: Option<&[usize]>) -> Result<RecordBatch> {
+    // Create a CSV reader. This is typically created on the thread that reads the file and
+    // thus owns the read head.
+    let mut reader = read::ReaderBuilder::new().delimiter(b'|').from_path(path)?;
+
+    // Infers the schema using the default inferer. The inferer is just a function that maps a string
+    // to a `DataType`.
+    let schema = read::infer_schema(&mut reader, None, true, &read::infer)?;
+
+    // allocate space to read from CSV to. The size of this vec denotes how many rows are read.
+    let mut rows = vec![read::ByteRecord::default(); 100];
+
+    // skip 0 (excluding the header) and read up to 100 rows.
+    // this is IO-intensive and performs minimal CPU work. In particular,
+    // no deserialization is performed.
+    let rows_read = read::read_rows(&mut reader, 0, &mut rows)?;
+    let rows = &rows[..rows_read];
+
+    // parse the batches into a `RecordBatch`. This is CPU-intensive, has no IO,
+    // and can be performed on a different thread by passing `rows` through a channel.
+    read::deserialize_batch(
+        rows,
+        schema.fields(),
+        projection,
+        0,
+        read::deserialize_column,
+    )
+}
+
+// Usage:
+//    % datautils -from A.csv -from_opts header=Y separator='|' -to A.parquet -to_opts
+
+fn main() -> Result<()> {
+    use std::env;
+    let args: Vec<String> = env::args().collect();
+
+    //let file_path = &args[1];
+    let file_path = "/Users/adarshrp/Projects/tpch-data/sf0.01/customer.tbl";
+
+    let batch = read_path(file_path, None)?;
+    println!("{:?}", batch);
+    Ok(())
 }
