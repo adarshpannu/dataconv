bc8112cc20d48b94fc086a98573ccfedc8a704c3
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2021-09-03 18:18:54 -0700

    Not quite working ... brb

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,13 @@ use arrow2::record_batch::RecordBatch;
 
 use std::env;
 use std::fs::File;
+
+#[macro_use]
 extern crate clap;
-use clap::{App, Arg, SubCommand};
+use clap::App;
+
+use arrow2::io::parquet::write::{write_file, to_parquet_schema, Compression, Encoding, Version, WriteOptions};
+use arrow2::io::parquet::write::{RowGroupIter, RowGroupIterator};
 
 struct CSV {
     filename: String,
@@ -46,62 +51,58 @@ impl CSV {
 }
 
 impl Iterator for CSV {
-    type Item = RecordBatch;
+    type Item = Result<RecordBatch>;
     fn next(&mut self) -> Option<Self::Item> {
         // skip 0 (excluding the header) and read up to 100 rows.
         // this is IO-intensive and performs minimal CPU work. In particular,
         // no deserialization is performed.
+        self.rows.clear();
         let rows_read = read::read_rows(&mut self.reader, 0, &mut self.rows).unwrap();
         let rows = &self.rows[..rows_read];
 
-        if rows_read == 0 {
-            return None;
+        if rows_read > 0 {
+            // parse the batches into a `RecordBatch`. This is CPU-intensive, has no IO,
+            // and can be performed on a different thread by passing `rows` through a channel.
+            Some(read::deserialize_batch(
+                rows,
+                self.schema.fields(),
+                None,
+                0,
+                read::deserialize_column,
+            ))
+        } else {
+            None
         }
-        // parse the batches into a `RecordBatch`. This is CPU-intensive, has no IO,
-        // and can be performed on a different thread by passing `rows` through a channel.
-        read::deserialize_batch(
-            rows,
-            self.schema.fields(),
-            None,
-            0,
-            read::deserialize_column,
-        )
-        .ok()
     }
 }
 
-fn setup_clp() -> App<'static, 'static> {
-    let app = App::new("fconv: file converter")
-        .version("1.0")
-        .author("Adarsh Pannu")
-        .about("Converts files from one format to another")
-        .subcommand(
-            SubCommand::with_name("from")
-                .about("specify input file details")
-                .arg(
-                    Arg::with_name("name")
-                        .long("name")
-                        .short("n")
-                        .help("specify filename")
-                        .takes_value(true)
-                        .required(true),
-                )
-                .arg(
-                    Arg::with_name("type")
-                        .long("type")
-                        .short("t")
-                        .help("specify file type (csv | [parquet)")
-                        .takes_value(true),
-                ),
-        );
-    app
+enum Filetype {
+    UNKNOWN,
+    CSV,
+    PARQUET,
+}
+
+fn get_filetype_from_name(filename: &str) -> Filetype {
+    let ix = filename.rfind(".");
+    if let Some(ix) = ix {
+        let file_ext = &filename[ix + 1..];
+        match file_ext.to_lowercase().as_str() {
+            "csv" | "CSV" => Filetype::CSV,
+            "parquet" => Filetype::PARQUET,
+            _ => Filetype::UNKNOWN,
+        }
+    } else {
+        Filetype::UNKNOWN
+    }
 }
 
 fn main() -> Result<()> {
     //let args: Vec<String> = env::args().collect();
     let arg_str = "fconv 
-        from --name /Users/adarshrp/Projects/flare/data/emp.csv \
-        to --name /tmp/emp.csv";
+        -f /Users/adarshrp/Projects/flare/data/emp.csv \
+        -t /tmp/emp.parquet 
+        -d , 
+        -h";
 
     let arg_vec: Vec<String> = arg_str
         .split(' ')
@@ -109,39 +110,68 @@ fn main() -> Result<()> {
         .filter(|e| e.len() > 0 && e != "\n")
         .collect();
 
-    let matches = setup_clp().get_matches_from(arg_vec);
+    //dbg!(&arg_vec);
 
-    println!("{:?}", matches);
+    // The YAML file is found relative to the current file, similar to how modules are found
+    let yaml = load_yaml!("clap.yml");
+    let matches = App::from_yaml(yaml).get_matches_from(arg_vec);
 
-    /*
-    let from_iter: Box<dyn Iterator<Item = RecordBatch>> = match clpr.get("from_type") {
-        Some("csv") => {
-            let delimiter = clpr.get("from_delimiter").unwrap_or("|");
+    let filetype = get_filetype_from_name(matches.value_of("from").unwrap());
+
+    let iter = match filetype {
+        Filetype::CSV => {
+            let delimiter = matches.value_of("delimiter").unwrap_or(",");
             if delimiter.len() != 1 {
-                panic!("Invalid from_delimiter specified.")
+                panic!("Invalid delimiter specified: >{}<.", delimiter)
             }
             let delimiter: u8 = delimiter.as_bytes()[0];
 
-            let has_header = clpr.get("from_has_header");
+            let has_header = matches.value_of("header");
             let has_header = match has_header {
                 Some("Y") | Some("y") => true,
                 _ => false,
             };
 
-            let filename = clpr.get("from").unwrap().to_string();
+            let filename = matches.value_of("from").unwrap().to_string();
             let csv = CSV::new(filename, has_header, delimiter);
             Box::new(csv)
         }
         _ => {
-            panic!("Invalid from_type: {:?}", clpr.get("from_type"))
+            panic!("Unknown filetype: {:?}", matches.value_of("from"))
         }
     };
 
+    /*
     for batch in from_iter {
         println!("{:?}", batch);
         //break;
     }
     */
+    // Create a new empty file
+    let output_filename = matches.value_of("to").unwrap();
 
+    let mut file = File::create(output_filename)?;
+    let options = WriteOptions {
+        write_statistics: true,
+        compression: Compression::Uncompressed,
+        version: Version::V2,
+    };
+    let encodings = vec![Encoding::Plain];
+    let schema = &iter.schema.clone();
+    let parquet_schema = to_parquet_schema(&iter.schema)?;
+
+    let iter: Box<dyn Iterator<Item = Result<RecordBatch>>> = iter;
+
+    let row_groups = RowGroupIterator::try_new(iter, schema, options, encodings)?;
+
+    // Write the file. Note that, at present, any error results in a corrupted file.
+    let _ = write_file(
+        &mut file,
+        row_groups,
+        &schema,
+        parquet_schema,
+        options,
+        None,
+    )?;
     Ok(())
 }
